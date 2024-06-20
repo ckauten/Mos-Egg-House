314f248af083f98f568b785897c95dcfbd25fa11
Cameron Kauten <cameron.kauten@gmail.com>
2024-06-20 13:56:40 -0700

    other pages added, nav bar added, structure started

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// const link = document.querySelector('a');
+
+// link.addEventListener('click', function () {
+//   if (link.classList.contains('current')) {
+//     link.classList.remove('current');
+//   } else {
+//     link.classList.add('current');
+//   }
+// });
