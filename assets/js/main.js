3e2514dcceea3da7e324a0219b5b4c3da484a6ef
Cameron Kauten <cameron.kauten@gmail.com>
2024-06-24 17:55:00 -0700

    dropdown working on desktop and mobile media screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,29 @@
-// const link = document.querySelector('a');
+const dropdownToggle = document.querySelector('#dropdown-toggle');
+const navUL = document.querySelector('#navUL');
+const menuButtons = document.querySelector('#menu-buttons');
 
-// link.addEventListener('click', function () {
-//   if (link.classList.contains('current')) {
-//     link.classList.remove('current');
-//   } else {
-//     link.classList.add('current');
-//   }
-// });
+dropdownToggle.addEventListener('click', function () {
+  if (dropdownToggle.classList.contains('show')) {
+    dropdownToggle.classList.remove('show');
+    dropdownToggle.style.transform = 'rotate(0deg)';
+    dropdownToggle.style.paddingRight = '0';
+    dropdownToggle.style.transition = 'transform 0.5s';
+    navUL.style.flexDirection = 'row';
+    navUL.style.flexWrap = 'nowrap';
+  } else {
+    dropdownToggle.classList.toggle('show');
+    dropdownToggle.style.paddingRight = '.5em';
+    dropdownToggle.style.transform = 'rotate(90deg)';
+    dropdownToggle.style.transition = 'transform 0.5s';
+    navUL.style.flexDirection = 'column';
+    navUL.style.justifyContent = 'flex-start';
+    navUL.style.maxHeight = '20em';
+    if (screen.width < 900) {
+      navUL.style.flexWrap = 'nowrap';
+    } else {
+      navUL.style.flexWrap = 'wrap';
+      navUL.style.overflowX = 'none';
+      menuButtons.marginBottom = '1em';
+    }
+  }
+});
