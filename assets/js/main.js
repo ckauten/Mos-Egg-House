3576d17fc731913a84ea4b690618dea844f75526
Cameron Kauten <cameron.kauten@gmail.com>
2024-07-01 12:21:41 -0700

    about page added with slideshow and 4 images

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,3 +63,29 @@ if (currentPage.endsWith('menu.html') || currentPage.endsWith('menu')) {
 function goHome() {
   window.location.href = '/';
 }
+
+// about page slideshow
+
+if (currentPage.endsWith('about.html') || currentPage.endsWith('about')) {
+  var slideIndex = 1;
+  showDivs(slideIndex);
+
+  function plusDivs(n) {
+    showDivs((slideIndex += n));
+  }
+
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName('mySlides');
+    if (n > x.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = x.length;
+    }
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = 'none';
+    }
+    x[slideIndex - 1].style.display = 'block';
+  }
+}
