bafe0dd2478969c16639b9c2c93f91c38bb191ab
Cameron Kauten <cameron.kauten@gmail.com>
2024-07-03 14:08:07 -0700

    firefox specific styling added

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,38 +3,51 @@ const navUL = document.querySelector('#navUL');
 const menuButtons = document.querySelectorAll('.menu-buttons');
 const currentPage = window.location.pathname;
 
-//Menu Page JS
-if (currentPage.endsWith('menu.html') || currentPage.endsWith('menu')) {
-  dropdownToggle.addEventListener('click', function () {
-    if (dropdownToggle.classList.contains('show')) {
-      dropdownToggle.classList.remove('show');
-      dropdownToggle.style.transform = 'rotate(0deg)';
-      dropdownToggle.style.paddingRight = '0';
-      dropdownToggle.style.transition = 'transform 0.5s';
-      navUL.style.flexDirection = 'row';
-      navUL.style.flexWrap = 'nowrap';
-      navUL.style.overflowX = 'auto';
-    } else {
-      dropdownToggle.classList.toggle('show');
-      dropdownToggle.style.paddingRight = '.5em';
-      dropdownToggle.style.transform = 'rotate(90deg)';
-      dropdownToggle.style.transition = 'transform 0.5s';
-      navUL.style.flexDirection = 'column';
-      navUL.style.justifyContent = 'flex-start';
-      navUL.style.maxHeight = '20em';
-      if (screen.width < 900) {
-        navUL.style.flexWrap = 'nowrap';
-      } else {
-        navUL.style.flexWrap = 'wrap';
-        navUL.style.overflowX = 'hidden';
-        menuButtons.forEach((button) => {
-          button.style.marginBottom = '1em';
-        });
-      }
-    }
-  });
+// Function to detect Firefox browser
+function isFirefox() {
+  return typeof InstallTrigger !== 'undefined';
+}
 
-  //return to top button
+// Menu Page JS
+if (currentPage.endsWith('menu.html') || currentPage.endsWith('menu')) {
+  if (!isFirefox()) {
+    // Add event listener for browsers other than Firefox
+    dropdownToggle.addEventListener('click', function () {
+      if (dropdownToggle.classList.contains('show')) {
+        dropdownToggle.classList.remove('show');
+        dropdownToggle.style.transform = 'rotate(0deg)';
+        dropdownToggle.style.paddingRight = '0';
+        dropdownToggle.style.transition = 'transform 0.5s';
+        navUL.style.flexDirection = 'row';
+        navUL.style.flexWrap = 'nowrap';
+        navUL.style.overflowX = 'auto';
+        navUL.style.overflowY = 'hidden';
+      } else {
+        dropdownToggle.classList.toggle('show');
+        dropdownToggle.style.paddingRight = '.5em';
+        dropdownToggle.style.transform = 'rotate(90deg)';
+        dropdownToggle.style.transition = 'transform 0.5s';
+        navUL.style.flexDirection = 'column';
+        navUL.style.justifyContent = 'flex-start';
+        navUL.style.maxHeight = '20em';
+        navUL.style.overflowY = 'auto';
+        navUL.style.overflowX = 'hidden';
+        if (screen.width < 900) {
+          navUL.style.flexWrap = 'nowrap';
+        } else {
+          navUL.style.flexWrap = 'wrap';
+          menuButtons.forEach((button) => {
+            button.style.marginBottom = '1em';
+          });
+        }
+      }
+    });
+  } else {
+    // Hide the dropdown toggle button for Firefox
+    dropdownToggle.style.display = 'none';
+  }
+
+  // Return to top button
   document.addEventListener('DOMContentLoaded', () => {
     const hiddenElement = document.getElementById('return-to-top');
 
@@ -64,8 +77,7 @@ function goHome() {
   window.location.href = '/';
 }
 
-// about page slideshow
-
+// About page slideshow
 if (currentPage.endsWith('about.html') || currentPage.endsWith('about')) {
   var slideIndex = 1;
   showDivs(slideIndex);
